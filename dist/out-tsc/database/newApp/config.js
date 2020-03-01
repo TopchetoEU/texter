3af1ef1399e203a2653ad348c4bea7917d6360b5
TopchetoEU <koko.venkov@gmail.com>
2020-03-02 01:56:38 +0200

    OMG

--- dist/out-tsc/database/newApp/config.js
+++
@@ -1,4 +0,0 @@
-export const Config = {
-    PasswordRegEx: /([A-Z]|[a-z]|[0-9]|[`~!@#$%^&*()\-=+\{\[\]\}\\\|;:'",<.>/?_ ]){8,32}/g,
-};
-//# sourceMappingURL=config.js.map
--- dist/out-tsc/database/newApp/server/routers/index.js
+++
@@ -1,5 +0,0 @@
-import { GetArticlesRouter } from "./articlesRouter";
-import { GetUsersRouter } from "./usersRouter";
-export const getArticlesRouter = GetArticlesRouter;
-export const getUsersRouter = GetUsersRouter;
-//# sourceMappingURL=index.js.map
